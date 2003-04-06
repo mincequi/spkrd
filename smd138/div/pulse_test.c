12c69d0aa77d3aaf12453750196e09e62ed81592
sumpan <>
2003-04-06 17:59:43 +0000

    fixade till pulse_test-programmet funkar nu med pointers

--- smd138/div/pulse_test.c
+++ smd138/div/pulse_test.c
@@ -1,26 +1,55 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <sys/neutrino.h>
+#include <time.h>
 
+#define REPLY_BUFFER_SIZE 10
+#define RECV_BUFFER_SIZE  10
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+int chid;
 
 void *thread1(void *data)
 {
+   struct _pulse pulse;
+   struct timespec t;
+   void *ptr;
+   int recvid;
+   void *addr;
+   
    printf("Thread 1 started\n");
+   
    while (1) {
-      sleep(1);
+      recvid = MsgReceivePulse(chid, &pulse, sizeof(pulse), NULL);
+      printf("Thread 1, message received: %#x\n", pulse.value.sival_ptr, recvid);
       
+      t = *((struct timespec *)(pulse.value.sival_int));
+      printf("Unix time: %d\n", t.tv_sec);
+      
+      sleep(1);
    }
 }
 
 int main() 
 {
+   int conid, result;
+   int value = 42;
+   struct timespec now;
+   
+   chid = ChannelCreate(0);
+   
+   
    pthread_create(NULL, NULL, &thread1, NULL);
    
-   printf("Main thread\n");
+   
+   conid = ConnectAttach(0, 0, chid, 0, 0); 
+   
+   printf("Main thread started\n");
+      
    
    while (1) {
+      clock_gettime(CLOCK_REALTIME, &now);
+      result = MsgSendPulse(conid, 1, 0, (int)(&now));
+      printf("Main thread, MsgPulseSend  result: %d\n", result);
       sleep(5);
    }
    
