86514383ffe6fdd00f1bd5ba4c8f929c204262ea
Gary Arnold <garnold@garyarnold.com>
2014-07-09 15:39:34 -0700

    ga - Minor name changes

--- src/mainwindow.cc
+++ src/mainwindow.cc
@@ -48,7 +48,7 @@ MainWindow::MainWindow()
     cout << fe.code() << endl;
 #endif
   }
-  set_title("GSpeakers-" + string(VERSION));
+  set_title("GSpeakers Redux-" + string(VERSION));
 
   /* add a vbox to the window */
   add(m_main_vbox);
@@ -363,7 +363,7 @@ void MainWindow::on_switch_page(GtkNotebookPage* page, guint page_num)
 
 void MainWindow::on_about()
 {
-  Gtk::MessageDialog m(*this, "GSpeakers-" + string(VERSION) + 
+  Gtk::MessageDialog m(*this, "GSpeakers Redux-" + string(VERSION) +
                        "\n\n(C) Daniel Sundberg <sumpan@sumpan.com>\n\nhttp://gspeakers.sf.net", false, 
                        Gtk::MESSAGE_INFO, Gtk::BUTTONS_OK, true);
   m.run();
