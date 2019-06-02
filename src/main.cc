a8c803c668a199cc8fdd8701e31943224c1c81b1
Darcy Beurle <darcy@beurle.id.au>
2019-06-02 18:48:01 +0200

    Clean up commented code

--- src/main.cc
+++ src/main.cc
@@ -55,26 +55,17 @@ int main(int argc, char* argv[]) {
     g_settings.load(Glib::get_home_dir() + "/.gspeakers/gspeakers2.conf");
 #endif
   }
+
   g_settings.defaultValueUnsignedInt("ToolbarStyle", Gtk::TOOLBAR_BOTH);
-  /* Init BoxEditor before BoxHistory, this will make BoxHistory signal BoxEditor with selected box,
-   * which is nice (tm) */
-  // BoxEditor be;
-  // BoxHistory bh;
 
-  /* Plot history will always be empty at startup for now */
-  // PlotHistory ph;
-
-  // GSpeakersBoxPlot gsbp;
   std::cout << _("Testing gettext") << std::endl;
-  // SpeakerListSelector sls;
+
   gspeakers_stock_init();
+
   MainWindow mw;
-  // mw.show_all_children();
-  // signal_plot_crossover();
 
   Gtk::Main::run(mw);
-  //  std::cout << c1;
-  /* save settings */
+
   try {
     g_settings.save();
   } catch (std::runtime_error const& e) {
@@ -82,89 +73,5 @@ int main(int argc, char* argv[]) {
     std::cout << "Main: " << e.what() << std::endl;
 #endif
   }
-
-  // Net n;
-  // n.set_has_imp_corr(true);
-  // n.set_has_damp(true);
-  // Speaker s;
-  // std::cout << n.to_SPICE(s) << std::endl;
-  // printf("%e\n", 3.0);
   return 0;
 }
-/*
-// test net
-Net n1;
-//  std::cout << n1 << "--------" << std::endl;
-  n1.set_highpass_order(NET_ORDER_1ST);
-  n1.set_lowpass_order(NET_ORDER_3RD);
-  (*n1.parts())[0].set_value(4.7);
-  (*n1.parts())[1].set_value(6.8);
-  (*n1.parts())[2].set_value(3.3);
-  (*n1.parts())[n1.get_lowpass_order() + 0].set_value(4.7);
-  std::cout << n1 << "--------" << std::endl;
-  n1.set_lowpass_order(NET_ORDER_1ST);
-  std::cout << n1 << "--------" << std::endl;
-  n1.set_highpass_order(NET_NOT_PRESENT);
-  n1.set_lowpass_order(NET_NOT_PRESENT);
-  n1.set_lowpass_order(NET_ORDER_2ND);
-  (*n1.parts())[1].set_value(6.8);
-  std::cout << n1;
-
-*/
-
-/*
- std::vector<string> v;
-  v.push_back("hejsan");
-  v.push_back("halloe");
-  v.push_back("halihalo");
-
- std::vector<string>::iterator iter = v.begin();
-
-  for(
-     std::vector<string>::iterator from = v.begin();
-      from != v.end();
-      ++from
-     )
-  {
-    std::cout << *from << std::endl;
-  }
-  v.erase(iter + 1);
-  for(
-     std::vector<string>::iterator from = v.begin();
-      from != v.end();
-      ++from
-     )
-  {
-    std::cout << *from << std::endl;
-  }
-
- std::vector<string> v;
-  v.push_back("hejsan");
-  v.push_back("halloe");
-  v.push_back("halihalo");
-
- std::vector<string>::iterator iter = v.begin();
-
-  for(
-     std::vector<string>::iterator from = v.begin();
-      from != v.end();
-      ++from
-     )
-  {
-    std::cout << *from << std::endl;
-  }
-  std::cout << "---" << std::endl;
-  iter = v.insert(iter + 1, "hej1");
-  iter = v.insert(iter, "hej2");
-  v.insert(iter, "hej3");
-  for(
-     std::vector<string>::iterator from = v.begin();
-      from != v.end();
-      ++from
-     )
-  {
-    std::cout << *from << std::endl;
-  }
-
-
-*/
