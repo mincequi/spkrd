bae9f77274eda675059679e920f5130bc80eaba4
Darcy Beurle <darcy@beurle.id.au>
2020-04-11 18:27:06 +0200

    Update method name to match interface

--- test/enclosure.cpp
+++ test/enclosure.cpp
@@ -7,7 +7,7 @@ std::string const path = GSPK_TEST_FILE_DIRECTORY;
 
 TEST_CASE("Populate enclosure list from xml")
 {
-    SECTION("Empty case") { REQUIRE(enclosure_list().box_list().empty()); }
+    SECTION("Empty case") { REQUIRE(enclosure_list().data().empty()); }
     SECTION("File not found exception")
     {
         REQUIRE_THROWS_AS(enclosure_list(""), std::runtime_error);
@@ -16,10 +16,10 @@ TEST_CASE("Populate enclosure list from xml")
     {
         std::puts(GSPK_TEST_FILE_DIRECTORY);
         auto enclosures = enclosure_list(path + "/enclosure.xml");
-        REQUIRE(enclosures.box_list().size() == 4);
+        REQUIRE(enclosures.data().size() == 4);
 
         {
-            auto const& enclosure = enclosures.box_list().at(0);
+            auto const& enclosure = enclosures.data().at(0);
             REQUIRE(enclosure.get_id_string() == "Box: Wed Sep 18 14:59:49 2002");
             REQUIRE(enclosure.get_type() == 2);
             REQUIRE(enclosure.get_vb1() == Approx(23));
