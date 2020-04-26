Dhar/gspeakers

camel case -> snake case

src/plot/plothistory.h
@@ -48,80 +48,91 @@
  * The list contains a list of all enclosures...you can select
  * to plot, not plot or whatever for each enclosure.
  */
-class PlotHistory : public Gtk::Frame {
+class PlotHistory : public Gtk::Frame
+{
 public:
-  PlotHistory();
+    PlotHistory();
 
-  ~PlotHistory() override;
-  void on_remove();
+    ~PlotHistory() override;
 
-protected:
-  /* callbacks */
-  void on_selection_changed();
-  void on_box_modified(Box* box);
-  void on_add_plot(Box* box, Speaker* speaker, Gdk::Color&);
-  void on_cell_plot_toggled(const Glib::ustring& path_string);
-  auto on_delete_event(GdkEventAny*) -> bool override;
-  void type_cell_data_func(Gtk::CellRenderer* cell, const Gtk::TreeModel::iterator& iter);
-  void vb1_cell_data_func(Gtk::CellRenderer* cell, const Gtk::TreeModel::iterator& iter);
-  void fb1_cell_data_func(Gtk::CellRenderer* cell, const Gtk::TreeModel::iterator& iter);
-
-  /* Helper member functions */
-  virtual void create_model();
-  virtual void add_columns();
-  virtual void add_item(Box const& box, Speaker const& spk, Gdk::Color&);
+    void on_remove();
 
 protected:
-  /* This is used in the treemodel */
-  struct ModelColumns : public Gtk::TreeModelColumnRecord {
-    Gtk::TreeModelColumn<int> id;
-    Gtk::TreeModelColumn<int> type;
-    Gtk::TreeModelColumn<Glib::ustring> id_string;
-    Gtk::TreeModelColumn<Glib::ustring> speaker_string;
-    Gtk::TreeModelColumn<std::string> color;
-    Gtk::TreeModelColumn<bool> view_plot;
-    Gtk::TreeModelColumn<double> vb1;
-    Gtk::TreeModelColumn<double> fb1;
-    Gtk::TreeModelColumn<double> vb2;
-    Gtk::TreeModelColumn<double> fb2;
-    Gtk::TreeModelColumn<Gdk::Color> color_;
-    Gtk::TreeModelColumn<Pango::Weight> weight_;
+    void on_selection_changed();
 
-    ModelColumns() {
-      add(id);
-      add(type);
-      add(id_string);
-      add(color);
-      add(view_plot);
-      add(vb1);
-      add(vb2);
-      add(fb1);
-      add(fb2);
-      add(color_);
-      add(weight_);
-      add(speaker_string);
-    }
-  };
+    void on_box_modified(Box* box);
+
+    void on_add_plot(Box* box, Speaker* speaker, Gdk::Color&);
+
+    void on_cell_plot_toggled(Glib::ustring const& path_string);
+
+    auto on_delete_event(GdkEventAny*) -> bool override;
+
+    void type_cell_data_func(Gtk::CellRenderer* cell, Gtk::TreeModel::iterator const& iter);
+
+    void vb1_cell_data_func(Gtk::CellRenderer* cell, Gtk::TreeModel::iterator const& iter);
+
+    void fb1_cell_data_func(Gtk::CellRenderer* cell, Gtk::TreeModel::iterator const& iter);
+
+    virtual void create_model();
+
+    virtual void add_columns();
+
+    virtual void add_item(Box const& box, Speaker const& spk, Gdk::Color&);
 
 protected:
-  // Member widgets:
-  Gtk::Table m_Table;
-  Gtk::ScrolledWindow m_ScrolledWindow;
-  Gtk::TreeView m_TreeView;
-  Glib::RefPtr<Gtk::ListStore> m_refListStore;
+    /// Used for the treemodel
+    struct model_columns : public Gtk::TreeModelColumnRecord
+    {
+        Gtk::TreeModelColumn<int> id;
+        Gtk::TreeModelColumn<int> type;
+        Gtk::TreeModelColumn<Glib::ustring> id_string;
+        Gtk::TreeModelColumn<Glib::ustring> speaker_string;
+        Gtk::TreeModelColumn<std::string> color;
+        Gtk::TreeModelColumn<bool> view_plot;
+        Gtk::TreeModelColumn<double> vb1;
+        Gtk::TreeModelColumn<double> fb1;
+        Gtk::TreeModelColumn<double> vb2;
+        Gtk::TreeModelColumn<double> fb2;
+        Gtk::TreeModelColumn<Gdk::Color> color_;
+        Gtk::TreeModelColumn<Pango::Weight> weight_;
 
-  Gtk::VBox m_vbox;
-  Gtk::Label m_label;
+        model_columns()
+        {
+            add(id);
+            add(type);
+            add(id_string);
+            add(color);
+            add(view_plot);
+            add(vb1);
+            add(vb2);
+            add(fb1);
+            add(fb2);
+            add(color_);
+            add(weight_);
+            add(speaker_string);
+        }
+    };
 
-  enclosure_list m_box_list;
-  speaker_list m_speaker_list;
+protected:
+    // Member widgets:
+    Gtk::Table m_Table;
+    Gtk::ScrolledWindow m_ScrolledWindow;
+    Gtk::TreeView m_TreeView;
+    Glib::RefPtr<Gtk::ListStore> m_refListStore;
 
-  GSpeakersColor m_color_list;
+    Gtk::VBox m_vbox;
+    Gtk::Label m_label;
 
-  ModelColumns m_columns;
+    enclosure_list m_box_list;
+    speaker_list m_speaker_list;
 
-  int m_index{0};
-  int m_nof_plots{0};
+    GSpeakersColor m_color_list;
+
+    model_columns m_columns;
+
+    int m_index{0};
+    int m_nof_plots{0};
 };
 
 #endif

mincequi/spkrd

src/plot/plothistory.h
@@ -48,80 +48,91 @@
  * The list contains a list of all enclosures...you can select
  * to plot, not plot or whatever for each enclosure.
  */
-class PlotHistory : public Gtk::Frame {
+class PlotHistory : public Gtk::Frame
+{
 public:
-  PlotHistory();
+    PlotHistory();
 
-  ~PlotHistory() override;
-  void on_remove();
+    ~PlotHistory() override;
 
-protected:
-  /* callbacks */
-  void on_selection_changed();
-  void on_box_modified(enclosure* box);
-  void on_add_plot(enclosure* box, driver* speaker, Gdk::Color&);
-  void on_cell_plot_toggled(const Glib::ustring& path_string);
-  auto on_delete_event(GdkEventAny*) -> bool override;
-  void type_cell_data_func(Gtk::CellRenderer* cell, const Gtk::TreeModel::iterator& iter);
-  void vb1_cell_data_func(Gtk::CellRenderer* cell, const Gtk::TreeModel::iterator& iter);
-  void fb1_cell_data_func(Gtk::CellRenderer* cell, const Gtk::TreeModel::iterator& iter);
-
-  /* Helper member functions */
-  virtual void create_model();
-  virtual void add_columns();
-  virtual void add_item(enclosure const& box, driver const& spk, Gdk::Color&);
+    void on_remove();
 
 protected:
-  /* This is used in the treemodel */
-  struct ModelColumns : public Gtk::TreeModelColumnRecord {
-    Gtk::TreeModelColumn<int> id;
-    Gtk::TreeModelColumn<int> type;
-    Gtk::TreeModelColumn<Glib::ustring> id_string;
-    Gtk::TreeModelColumn<Glib::ustring> speaker_string;
-    Gtk::TreeModelColumn<std::string> color;
-    Gtk::TreeModelColumn<bool> view_plot;
-    Gtk::TreeModelColumn<double> vb1;
-    Gtk::TreeModelColumn<double> fb1;
-    Gtk::TreeModelColumn<double> vb2;
-    Gtk::TreeModelColumn<double> fb2;
-    Gtk::TreeModelColumn<Gdk::Color> color_;
-    Gtk::TreeModelColumn<Pango::Weight> weight_;
+    void on_selection_changed();
 
-    ModelColumns() {
-      add(id);
-      add(type);
-      add(id_string);
-      add(color);
-      add(view_plot);
-      add(vb1);
-      add(vb2);
-      add(fb1);
-      add(fb2);
-      add(color_);
-      add(weight_);
-      add(speaker_string);
-    }
-  };
+    void on_box_modified(enclosure* box);
+
+    void on_add_plot(enclosure* box, driver* speaker, Gdk::Color&);
+
+    void on_cell_plot_toggled(Glib::ustring const& path_string);
+
+    auto on_delete_event(GdkEventAny*) -> bool override;
+
+    void type_cell_data_func(Gtk::CellRenderer* cell, Gtk::TreeModel::iterator const& iter);
+
+    void vb1_cell_data_func(Gtk::CellRenderer* cell, Gtk::TreeModel::iterator const& iter);
+
+    void fb1_cell_data_func(Gtk::CellRenderer* cell, Gtk::TreeModel::iterator const& iter);
+
+    virtual void create_model();
+
+    virtual void add_columns();
+
+    virtual void add_item(enclosure const& box, driver const& spk, Gdk::Color&);
 
 protected:
-  // Member widgets:
-  Gtk::Table m_Table;
-  Gtk::ScrolledWindow m_ScrolledWindow;
-  Gtk::TreeView m_TreeView;
-  Glib::RefPtr<Gtk::ListStore> m_refListStore;
+    /// Used for the treemodel
+    struct model_columns : public Gtk::TreeModelColumnRecord
+    {
+        Gtk::TreeModelColumn<int> id;
+        Gtk::TreeModelColumn<int> type;
+        Gtk::TreeModelColumn<Glib::ustring> id_string;
+        Gtk::TreeModelColumn<Glib::ustring> speaker_string;
+        Gtk::TreeModelColumn<std::string> color;
+        Gtk::TreeModelColumn<bool> view_plot;
+        Gtk::TreeModelColumn<double> vb1;
+        Gtk::TreeModelColumn<double> fb1;
+        Gtk::TreeModelColumn<double> vb2;
+        Gtk::TreeModelColumn<double> fb2;
+        Gtk::TreeModelColumn<Gdk::Color> color_;
+        Gtk::TreeModelColumn<Pango::Weight> weight_;
 
-  Gtk::VBox m_vbox;
-  Gtk::Label m_label;
+        model_columns()
+        {
+            add(id);
+            add(type);
+            add(id_string);
+            add(color);
+            add(view_plot);
+            add(vb1);
+            add(vb2);
+            add(fb1);
+            add(fb2);
+            add(color_);
+            add(weight_);
+            add(speaker_string);
+        }
+    };
 
-  enclosure_list m_box_list;
-  driver_list m_speaker_list;
+protected:
+    // Member widgets:
+    Gtk::Table m_Table;
+    Gtk::ScrolledWindow m_ScrolledWindow;
+    Gtk::TreeView m_TreeView;
+    Glib::RefPtr<Gtk::ListStore> m_refListStore;
 
-  colours m_color_list;
+    Gtk::VBox m_vbox;
+    Gtk::Label m_label;
 
-  ModelColumns m_columns;
+    enclosure_list m_box_list;
+    driver_list m_speaker_list;
 
-  int m_index{0};
-  int m_nof_plots{0};
+    colours m_color_list;
+
+    model_columns m_columns;
+
+    int m_index{0};
+    int m_nof_plots{0};
 };
 
 #endif