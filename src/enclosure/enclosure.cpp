Dhar/gspeakers

Trailing return type

src/enclosure/enclosure.cpp
@@ -80,7 +80,7 @@ Box::Box(xmlNodePtr parent) : GSpeakersObject()
     }
 }
 
-xmlNodePtr Box::to_xml_node(xmlNodePtr parent)
+auto Box::to_xml_node(xmlNodePtr parent) -> xmlNodePtr
 {
     std::array<char, 8> buffer;
 
@@ -104,7 +104,7 @@ xmlNodePtr Box::to_xml_node(xmlNodePtr parent)
     return box;
 }
 
-std::ostream& operator<<(std::ostream& output, const Box& box)
+auto operator<<(std::ostream& output, const Box& box) -> std::ostream&
 {
     output << _("Box") << "\n"
            << _("Id:        ") << box.m_id << "\n"

mincequi/spkrd

src/enclosure/enclosure.cpp
@@ -86,7 +86,7 @@ enclosure::enclosure(xmlNodePtr parent) : GSpeakersObject()
     }
 }
 
-xmlNodePtr enclosure::to_xml_node(xmlNodePtr parent)
+auto enclosure::to_xml_node(xmlNodePtr parent) -> xmlNodePtr
 {
     std::array<char, 8> buffer;
 
@@ -110,7 +110,7 @@ xmlNodePtr enclosure::to_xml_node(xmlNodePtr parent)
     return box;
 }
 
-std::ostream& operator<<(std::ostream& output, const enclosure& box)
+auto operator<<(std::ostream& output, enclosure const& box) -> std::ostream&
 {
     output << _("enclosure") << "\n"
            << _("Id:        ") << box.m_id << "\n"