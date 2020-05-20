afb9d22e6952537bcc2cee981c7c76be1c66c9fc
Darcy Beurle <darcy@beurle.id.au>
2020-05-20 20:36:27 +0200

    Include the point lerp algorithm alongside lerp

--- src/plot/point.hpp
+++ src/plot/point.hpp
@@ -1,6 +1,9 @@
 
 #pragma once
 
+#include <vector>
+#include <algorithm>
+
 namespace gspk
 {
 /// Point class to exchange plot coordinates between classes.
@@ -41,5 +44,36 @@ struct comparison
         return left <= right.get_x();
     }
 };
+
+inline auto lerp(std::vector<point> const& freq_resp_points, double x) -> double
+{
+    auto first_i = std::lower_bound(freq_resp_points.begin(),
+                                    freq_resp_points.end(),
+                                    x,
+                                    gspk::comparison{});
+
+    if (first_i != begin(freq_resp_points))
+    {
+        std::advance(first_i, -1);
+    }
+
+    auto second_i = std::upper_bound(begin(freq_resp_points),
+                                     end(freq_resp_points),
+                                     x,
+                                     gspk::comparison{});
+
+    double x0 = first_i->get_x();
+    double x1 = second_i->get_x();
+
+    if (x0 == x1)
+    {
+        return first_i->get_y();
+    }
+
+    double y0 = first_i->get_y();
+    double y1 = second_i->get_y();
+
+    return y0 + (y1 - y0) * (x - x0) / (x1 - x0);
+}
 
 }
